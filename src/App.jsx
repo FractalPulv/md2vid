64c11ed2ecaa6c1f33c2352e1b53e101e7981133
FractalPulv <luca.pul@icloud.com>
2024-05-14 16:47:38 +0200

    Tauri now working (basic functionality)

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import { invoke } from "@tauri-apps/api";
+
 function App() {
   const [count, setCount] = useState(0);
+
+  // now we can call our Command!
+  // Right-click the application background and open the developer tools.
+  // You will see "Hello, World!" printed in the console!
+  invoke("greet", { name: "World" })
+    // `invoke` returns a Promise
+    .then((response) => console.log(response));
 
   return (
     <>
